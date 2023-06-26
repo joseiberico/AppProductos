4815f4717c7765ee57a7c9ccc9aa5ac945f816f6
ciberico93 <ciberico93@gmail.com>
2023-06-25 20:06:39 -0500

    Actualizacion checalito

--- ApiProductos/ApiProductos/Models/Categoria.cs
+++ ApiProductos/ApiProductos/Models/Categoria.cs
@@ -4,6 +4,7 @@ namespace ApiProductos.Models
 {
     public class Categoria
     {
+        //Data Anotation
         [Key]
         public int Id_Categoria { get; set; }
         public string Nombre { get; set; }
